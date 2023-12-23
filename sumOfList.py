f00dc0c8214d45ef475210a691b98130e8adc6e4
pavishwaaa <pavishwa80@gmail.com>
2023-12-23 15:03:46 -0500

    Sum of Numbers in list

--- sumOfList.py
+++ sumOfList.py
@@ -1,2 +1,13 @@
 # Objective: Create a program that
 # calculates the sum of all elements in a list.
+
+numList = []
+
+num = int(input("Enter number of element in list:"))
+
+for i in range(0, num):
+    element = int(input("Enter elements:"))
+    numList.append(element)
+
+print("Largest element in the list: ", sum(numList))
+

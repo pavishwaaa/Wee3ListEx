8b83be489bb32d435422d8115d8f8976100dab18
pavishwaaa <pavishwa80@gmail.com>
2023-12-23 21:59:23 -0500

    Filter odd and even number from list

---
+++ FilterEvenNum.py
@@ -0,0 +1,26 @@
+# Objective: Develop a program that filters out
+# odd numbers
+# from a list and prints the remaining even numbers.
+
+numList = []
+
+num = int(input("Enter number of element in list:"))
+
+for i in range(0, num):
+    element = int(input("Enter elements:"))
+    numList.append(element)
+
+
+def is_odd(num):
+    return num % 2 != 0
+
+
+def is_even(num):
+    return num % 2 == 0
+
+
+oddNum = list(filter(is_odd, numList))
+evenNum = list(filter(is_even, numList))
+
+print("Odd numbers:", oddNum)
+print("Even numbers:", evenNum)
